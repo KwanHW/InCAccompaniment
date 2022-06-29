1e2c34d13fddc25e80828dc357e11151c23c4533
kwanhw <kwanhaowei@gmail.com>
2022-06-29 10:51:44 +0800

    Add subdivision selection

--- src/App.js
+++ src/App.js
@@ -11,40 +11,49 @@ import {
   SliderFilledTrack,
   SliderThumb,
   Button,
+  SliderMark,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 // import { Logo } from './Logo';
 import * as Tone from 'tone';
-import PianoC4 from './audio/piano-C4.wav';
-// import PianoC5 from './audio/piano-C5.wav';
-// import PianoC6 from './audio/piano-C6.wav';
+import PianoC4 from './audio/c4.mp3';
 
 // const synth = new Tone.Synth().toDestination()
 const sound = new Tone.Player(PianoC4).toDestination();
 let transport = Tone.Transport;
+let audioContextStarted = false;
 function App() {
   const [bpm, setBpm] = useState(60);
+  const [subdivision, setSubdivision] = useState('4n');
   const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     transport.stop();
     if (isPlaying) {
+      transport.cancel();
       transport = Tone.Transport.set({ bpm });
       transport.scheduleRepeat(time => {
-        sound.start(time, undefined, bpm / 60);
-      }, '4n');
+        sound.start(time);
+      }, subdivision);
       transport.start();
     }
-  }, [bpm]);
+  }, [bpm, subdivision, isPlaying]);
 
   const playAudioHandler = () => {
+    if (!audioContextStarted) {
+      Tone.start();
+      audioContextStarted = true;
+    }
+
     if (isPlaying) {
       transport.stop();
     } else {
+      transport.cancel();
       transport = Tone.Transport.set({ bpm });
       transport.scheduleRepeat(time => {
-        sound.start();
-      }, '4n');
+        console.log(time);
+        sound.start(time);
+      }, subdivision);
       transport.start();
     }
     setIsPlaying(!isPlaying);
@@ -67,11 +76,11 @@ function App() {
         <VStack spacing={3}>
           <Text fontSize="2xl">BPM:</Text>
           <Flex direction="row">
-            <Button onClick={increaseBPMHandler}>+</Button>
+            <Button onClick={decreaseBPMHandler}>-</Button>
             <Text fontSize="xl" px={7}>
               {bpm}
             </Text>
-            <Button onClick={decreaseBPMHandler}>-</Button>
+            <Button onClick={increaseBPMHandler}>+</Button>
           </Flex>
           <Slider
             aria-label="slider-1"
@@ -80,14 +89,38 @@ function App() {
             min={45}
             max={200}
             step={1}
+            size="lg"
+            colorScheme="teal"
             onChange={val => setBpm(val)}
           >
             <SliderTrack>
               <SliderFilledTrack />
             </SliderTrack>
-            <SliderThumb />
+            <SliderThumb boxSize={6} />
           </Slider>
-          <Button onClick={playAudioHandler}>{isPlaying ? 'Stop' : 'Play'}</Button>
+          <Button onClick={playAudioHandler} colorScheme={isPlaying ? 'red' : 'teal'}>
+            {isPlaying ? 'Stop' : 'Play'}
+          </Button>
+          <Flex direction="row">
+            <Button
+              onClick={() => setSubdivision('4n')}
+              colorScheme={subdivision === '4n' ? 'teal' : 'gray'}
+            >
+              Crotchet
+            </Button>
+            <Button
+              onClick={() => setSubdivision('8n')}
+              colorScheme={subdivision === '8n' ? 'teal' : 'gray'}
+            >
+              Quaver
+            </Button>
+            <Button
+              onClick={() => setSubdivision('16n')}
+              colorScheme={subdivision === '16n' ? 'teal' : 'gray'}
+            >
+              Semiquaver
+            </Button>
+          </Flex>
           {/* <Button onClick={decreaseBPMHandler}>Less BPM</Button> */}
           {/* <Logo h="40vmin" pointerEvents="none" />
             <Text>
